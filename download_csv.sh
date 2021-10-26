622cf948986404d0dd4a699a398f5e8d7ab813b8
Piotr Palka <piotr.palka123@gmail.com>
2021-10-26 23:06:35 +0200

    remove unzipping

--- download_csv.sh
+++ download_csv.sh
@@ -17,14 +17,20 @@ URL_BASE='http://data.gdeltproject.org/gdeltv2/'
 FILE_SUFFIX='00.export.CSV.zip' # we will ignore seconds from timestamp
 # to avoid human mistake if user will prvide some seconds on the end
 TRESHOLD=15
- 
-from='2021-10-24 15:30:00'
-to='2021-10-24 16:30:00'
+CSV_FILES_CATALOG=csv_files
+ZIP_FILES_CATALOG=zip_files
+
+from='2020-10-26 15:00:00'
+to='2021-10-26 15:00:00'
  
 # Let's convert provided dates to timestamps
 timestamp_from=$(date -d "$from" +'%Y%m%d%H%M')
 timestamp_to=$(date -d "$to" +'%Y%m%d%H%M')
  
+# mkdir -p $CSV_FILES_CATALOG
+mkdir -p $ZIP_FILES_CATALOG
+cd $ZIP_FILES_CATALOG
+
 # We're going to add 15 to "from date" and download the file in the loop.
 # Eventually we'll get to the point where "from date" is bigger than "to date" - that
 # means we have to stop. 
@@ -33,8 +39,10 @@ while [ $timestamp_from -le $timestamp_to ]; do
     echo "Downloading file ${timestamp_from}${FILE_SUFFIX}"
  
     # Download file
-    wget ${URL_BASE}${timestamp_from}${FILE_SUFFIX}
-    unzip ${timestamp_from}${FILE_SUFFIX}
+    wget --no-clobber ${URL_BASE}${timestamp_from}${FILE_SUFFIX}
+    # unzip ${timestamp_from}${FILE_SUFFIX} 
+    # -d ../$CSV_FILES_CATALOG
+    # rm ${timestamp_from}${FILE_SUFFIX}
  
     # Increment the date
     from=$(date -d "$from $TRESHOLD mins")
